caa50c216ac574ff4ec357f3c9c5a484c8573bc2
omardallah68 <omardallah68@users.noreply.github.com>
2023-03-03 21:12:51 +0100

    correct

--- public/script.js
+++ public/script.js
@@ -1,80 +1,75 @@
-const header= document.querySelector("header")
-const first= document.querySelector(".first")
-const aboutMe= document.querySelector(".aboutMe")
-const turn= document.querySelector(".turn")
+const header = document.querySelector("header");
+const first = document.querySelector(".first");
+const aboutMe = document.querySelector(".aboutMe");
+const turn = document.querySelector(".turn");
 
-
-document.addEventListener('scroll', ()=>{ 
+document.addEventListener("scroll", () => {
   const vh = window.pageYOffset;
-  if (vh>=100){
-  header.style.transform= "translateY(0px)"
+  if (vh >= 100) {
+    header.style.transform = "translateY(0px)";
   }
-  if (vh<100){
-  header.style.transform= "translateY(-100px)"
+  if (vh < 100) {
+    header.style.transform = "translateY(-100px)";
   }
-  if (vh>=300){
-    first.style.transform= "translateY(300px)"
+  if (vh >= 300) {
+    first.style.transform = "translateY(300px)";
     /*first.style.textShadow= "#f0c31f 0px 1px 3px"*/
-    first.style.textShadow= "0px 0px #888, 0px 0px #666, 0px 0px #444, 0px 0px #222"
-    first.style.backgroundColor="#000000cc"
+    first.style.textShadow =
+      "0px 0px #888, 0px 0px #666, 0px 0px #444, 0px 0px #222";
+    first.style.backgroundColor = "#000000cc";
   }
-  if (vh<300){
-    first.style.transform= "rotateX(315deg) rotateZ(45deg)"
-    first.style.textShadow= "1px 1px #888, 2px 2px #666, 3px 3px #444, 4px 4px #222"
-    first.style.backgroundColor="transparent"
+  if (vh < 300) {
+    first.style.transform = "rotateX(315deg) rotateZ(45deg)";
+    first.style.textShadow =
+      "1px 1px #888, 2px 2px #666, 3px 3px #444, 4px 4px #222";
+    first.style.backgroundColor = "transparent";
   }
-})
+});
 
+const card = document.querySelectorAll(".card");
+const turnIcon = document.querySelector(".turn span");
 
-const card=document.querySelectorAll(".card")
-const turnIcon=document.querySelector(".turn span")
-
-turn.addEventListener("click",() => {
-  card.forEach(i => {
-    const cardDetails=i.querySelectorAll(".cardDetails")
-    const cardTitle = i.querySelectorAll(".cardTitle")
-    cardDetails.forEach(f=>{
-      cardTitle.forEach(x=>{
-        if(f.style.display === "flex"){
-          f.style.display = "none"
-          x.style.display = "flex"
-          i.style.boxShadow="inset 0px 0px 1px 0px #f0c31f"
+turn.addEventListener("click", () => {
+  card.forEach((i) => {
+    const cardDetails = i.querySelectorAll(".cardDetails");
+    const cardTitle = i.querySelectorAll(".cardTitle");
+    cardDetails.forEach((f) => {
+      cardTitle.forEach((x) => {
+        if (f.style.display === "flex") {
+          f.style.display = "none";
+          x.style.display = "flex";
+          i.style.boxShadow = "inset 0px 0px 1px 0px #f0c31f";
           /*turnIcon.className="fa-regular fa-face-flushed"*/
-        }else{
-          f.style.display = "flex"
-          x.style.display = "flex"
-          i.style.boxShadow="inset 0px 0px 50px -10px #f0c31f"
+        } else {
+          f.style.display = "flex";
+          x.style.display = "flex";
+          i.style.boxShadow = "inset 0px 0px 50px -10px #f0c31f";
           /*turnIcon.className="fa-solid fa-face-flushed"*/
         }
-      })
-    })
-  })
-})
+      });
+    });
+  });
+});
 
-
-card.forEach(elem => {
-  const cardDetails=elem.querySelectorAll(".cardDetails")
-  const cardTitle = elem.querySelectorAll(".cardTitle")
-  elem.addEventListener("click",() => { 
-    cardDetails.forEach(elm =>{
-      cardTitle.forEach(el =>{
-        if (elm.style.display === "flex"){
-          elm.style.display = "none"
-          el.style.display = "flex"
-          elem.style.boxShadow="inset 0px 0px 1px 0px #f0c31f"
-        }else{
-          elm.style.display = "flex"
-          el.style.display = "none"
-          elem.style.boxShadow="inset 0px 0px 50px -10px #f0c31f"
+card.forEach((elem) => {
+  const cardDetails = elem.querySelectorAll(".cardDetails");
+  const cardTitle = elem.querySelectorAll(".cardTitle");
+  elem.addEventListener("click", () => {
+    cardDetails.forEach((elm) => {
+      cardTitle.forEach((el) => {
+        if (elm.style.display === "flex") {
+          elm.style.display = "none";
+          el.style.display = "flex";
+          elem.style.boxShadow = "inset 0px 0px 1px 0px #f0c31f";
+        } else {
+          elm.style.display = "flex";
+          el.style.display = "none";
+          elem.style.boxShadow = "inset 0px 0px 50px -10px #f0c31f";
         }
-        
-      })
-    })
-  })
-  
-})
-
-
+      });
+    });
+  });
+}); /*
 
 /*
 document.querySelectorAll(".card").forEach(elem =>
